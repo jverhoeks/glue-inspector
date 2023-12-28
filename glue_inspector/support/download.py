4d1482ea16345eb71f9f867689347f9703af27d3
Jacob Verhoeks <jjverhoeks@schubergphilis.com>
2023-12-28 11:13:19 +0100

    testing precommit

--- glue_inspector/support/download.py
+++ glue_inspector/support/download.py
@@ -1,11 +1,13 @@
+import logging
+
 import requests
 from bs4 import BeautifulSoup
-from collections import defaultdict
-import logging
 
 
 class GlueProvidedPackage:
-    url_glueetl = "https://docs.aws.amazon.com/glue/latest/dg/aws-glue-programming-python-libraries.html"
+    url_glueetl = (
+        "https://docs.aws.amazon.com/glue/latest/dg/aws-glue-programming-python-libraries.html"
+    )
     url_pythonshell = "https://docs.aws.amazon.com/glue/latest/dg/add-job-python.html"
 
     def __init__(self, cached=True):
@@ -20,48 +22,48 @@ class GlueProvidedPackage:
             "3.9",
         ]
 
-    def get(self, type, version):
+    def get(self, jobtype, version):
         print(self.packages_list)
-        if f"{type}-{version}" not in self.packages_list == 0:
+        if f"{jobtype}-{version}" not in self.packages_list == 0:
             if self.cached:
-                self.__read(type, version)
+                self.__read(jobtype, version)
             else:
-                self.download(type)
+                self.download(jobtype)
 
-        return self.packages_list[f"{type}-{version}"]
+        return self.packages_list[f"{jobtype}-{version}"]
 
-    def get_dict(self, type, version):
+    def get_dict(self, jobtype, version):
         print(self.packages_dict)
-        if f"{type}-{version}" not in self.packages_dict:
+        if f"{jobtype}-{version}" not in self.packages_dict:
             if self.cached:
-                self.__read(type, version)
+                self.__read(jobtype, version)
             else:
-                self.download(type)
-        return self.packages_dict[f"{type}-{version}"]
+                self.download(jobtype)
+        return self.packages_dict[f"{jobtype}-{version}"]
 
-    def __convert2dict(self, type, version):
+    def __convert2dict(self, jobtype, version):
         # convert into dict
-        self.packages_dict[f"{type}-{version}"] = {
+        self.packages_dict[f"{jobtype}-{version}"] = {
             p.split("==")[0]: {version: p.split("==")[1]}
-            for p in self.packages_list[f"{type}-{version}"]
+            for p in self.packages_list[f"{jobtype}-{version}"]
         }
 
-    def download(self, type):
+    def download(self, jobtype):
         """_summary_
 
         Args:
-            type (_type_): _description_
+            jobtype (_jobtype_): _description_
         """
-        if type == "pythonshell":
+        if jobtype == "pythonshell":
             return self.__download_pythonshell()
-        elif type == "glueetl":
+        elif jobtype == "glueetl":
             return self.__download_glueetl()
         else:
-            logging.error("Invalid Type")
+            logging.error("Invalid jobtype")
         return False
 
     def __download_glueetl(self):
-        response = requests.get(self.url_glueetl)
+        response = requests.get(self.url_glueetl, timeout=(15, 15))
         logging.debug(f"Download from {self.url_glueetl}")
         # Check if the request was successful (status code 200)
         if response.status_code == 200:
@@ -74,9 +76,7 @@ class GlueProvidedPackage:
                     target_div = soup.find("dd", {"tab-id": tabid})
                     # Extract packages with '==' from the list items
                     self.packages_list[f"glueetl-{version}"] = [
-                        li.get_text()
-                        for li in target_div.find_all("li")
-                        if "==" in li.get_text()
+                        li.get_text() for li in target_div.find_all("li") if "==" in li.get_text()
                     ]
 
                     self.__convert2dict("glueetl", version)
@@ -92,7 +92,7 @@ class GlueProvidedPackage:
         return False
 
     def __download_pythonshell(self):
-        response = requests.get(self.url_pythonshell)
+        response = requests.get(self.url_pythonshell, timeout=(15, 15))
         logging.debug(f"Download from {self.url_pythonshell}")
         # Check if the request was successful (status code 200)
         if response.status_code == 200:
@@ -107,9 +107,7 @@ class GlueProvidedPackage:
 
                 for row in target_table.find_all("tr"):
                     # Extract the text from each cell in the row
-                    row_data = [
-                        cell.get_text(strip=True) for cell in row.find_all(["td"])
-                    ]
+                    row_data = [cell.get_text(strip=True) for cell in row.find_all(["td"])]
                     table_data.append(row_data)
 
                 # remove first 2 items
@@ -120,9 +118,7 @@ class GlueProvidedPackage:
                 for index, version in enumerate(self.pythonshell_versions):
                     print(index, version)
                     self.packages_list[f"pythonshell-{version}"] = [
-                        f"{t[0]}=={t[index+1]}"
-                        for t in table_data
-                        if len(t[index + 1]) > 0
+                        f"{t[0]}=={t[index+1]}" for t in table_data if len(t[index + 1]) > 0
                     ]
                     print(f"pythonshell-{version}")
                     print(self.packages_list[f"pythonshell-{version}"])
@@ -136,25 +132,25 @@ class GlueProvidedPackage:
 
         return False
 
-    def __make_cache_file(self, type, version):
-        return f"{self.cache_prefix}-{type}-{version}.txt"
+    def __make_cache_file(self, jobtype, version):
+        return f"{self.cache_prefix}-{jobtype}-{version}.txt"
 
-    def __save(self, type, version):
-        cache_file = self.__make_cache_file(type, version)
-        if len(self.packages_list[f"{type}-{version}"]) > 0:
+    def __save(self, jobtype, version):
+        cache_file = self.__make_cache_file(jobtype, version)
+        if len(self.packages_list[f"{jobtype}-{version}"]) > 0:
             # Open the file in write mode
             logging.debug(f"Writing to {cache_file}")
             with open(cache_file, "w") as file:
-                file.write("\n".join(self.packages_list[f"{type}-{version}"]))
+                file.write("\n".join(self.packages_list[f"{jobtype}-{version}"]))
 
-    def __read(self, type, version):
-        cache_file = self.__make_cache_file(type, version)
+    def __read(self, jobtype, version):
+        cache_file = self.__make_cache_file(jobtype, version)
         print(cache_file)
         try:
             logging.debug(f"Reading from {cache_file}")
             with open(cache_file, "r") as file:
-                self.packages_list[f"{type}-{version}"] = file.read().split("\n")
-                self.__convert2dict(type, version)
+                self.packages_list[f"{jobtype}-{version}"] = file.read().split("\n")
+                self.__convert2dict(jobtype, version)
         except Exception as e:
             logging.error(f"Writing to {e}")
-            self.download(type)
+            self.download(jobtype)
